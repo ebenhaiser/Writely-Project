fbbe4cd06acbad8bc1e0ba27fd0d7c495df906fe
Ebenhaiser Caprisiano <ebenhaiser@gmail.com>
2025-02-08 16:07:57 +0700

    creating home post cards

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function home()
     {
-        return view('home');
+        $posts = Post::latest()->get();
+        return view('home', compact('posts'));
     }
 }
