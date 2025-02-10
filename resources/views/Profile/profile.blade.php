fb26f7b38bb63e0a0e88ff6756a8805f2b146c2b
Ebenhaiser Caprisiano <ebenhaiser@gmail.com>
2025-02-10 18:55:32 +0700

    rearrange components

--- resources/views/Profile/profile.blade.php
+++ resources/views/Profile/profile.blade.php
@@ -138,5 +138,15 @@
             </div>
         </div>
     </div>
-    @include('Profile.post')
+    <div class="row">
+        @forelse ($posts as $post)
+            <div class="col-md-4">
+                <x-cards.post-mini :post="$post" />
+            </div>
+        @empty
+            <div class="col-md-12" align="center">
+                <i>No post yet.</i>
+            </div>
+        @endforelse
+    </div>
 </x-layout>
