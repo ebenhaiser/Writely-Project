db5a864639a9c51043e6f008c046c4450abc5790
Ebenhaiser Caprisiano <ebenhaiser@gmail.com>
2025-02-10 18:54:39 +0700

    rearrange components

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -11,3 +11,4 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Monomaniac+One&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('css/style.css') }}">
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,12 +1,4 @@
 <x-layout>
-    <style>
-        .follow-profile img {
-            width: 50px;
-            height: 50px;
-            object-fit: cover;
-            border-radius: 50%;
-        }
-    </style>
     <div class="card mb-3">
         <div class="card-body d-flex gap-3">
             <input type="text" class="form-control" placeholder="Have a question? Ask Now">
@@ -17,41 +9,8 @@
         <h3 class="mb-3">Users:</h3>
         <div class="row">
             @foreach ($users as $user)
-                <div class="col-md-6 card follow-profile">
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between">
-                            <a href="{{ route('profile', $user->username) }}"
-                                style="color: inherit; text-decoration: none;">
-                                <span class="d-flex">
-                                    <span>
-                                        <div class="me-2">
-                                            <img src="{{ asset('img/profilePicture/' . ($user->profile_picture && file_exists(public_path('img/profilePicture/' . $user->profile_picture)) ? $user->profile_picture : 'default.jpg')) }}"
-                                                alt=""
-                                                class="rounded-circle border border-4 border-white-color-40">
-                                        </div>
-                                    </span>
-                                    <span class="my-auto">
-                                        <h6 class="mt-0 mb-0">{{ $user->name }}</h6>
-                                        <p class="mb-0 mt-0 text-body" style="text-decoration: none">
-                                            {{ '@' . $user->username }}</p>
-                                    </span>
-                                </span>
-                            </a>
-                            <span class="my-auto">
-                                <div align="right">
-                                    @if (!Auth::check() || Auth::id() !== $user->id)
-                                        <button class="btn btn-outline-primary follow-btn"
-                                            data-user-id="{{ $user->id }}">
-                                            <span
-                                                class="follow-text">{{ $user->isFollowedByUser() ? 'Unfollow' : 'Follow' }}</span>
-                                        </button>
-                                    @else
-                                        <p class="my-auto text-muted">You</p>
-                                    @endif
-                                </div>
-                            </span>
-                        </div>
-                    </div>
+                <div class="col-md-6">
+                    <x-cards.user :user="$user" />
                 </div>
             @endforeach
         </div>
